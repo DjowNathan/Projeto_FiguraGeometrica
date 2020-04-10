d6dbca18760382db5806e759773eeeb3377cd63c
Jonathan Araujo <57550980+DjowNathan@users.noreply.github.com>
2020-04-10 16:15:36 -0300

    calculo retangulo

--- src/Retangulo.java
+++ src/Retangulo.java
@@ -3,7 +3,6 @@ public class Retangulo extends Poligono implements Diagonal {
 
 	public Retangulo(double base, double altura) {
 		super(base, altura);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -14,8 +13,12 @@ public class Retangulo extends Poligono implements Diagonal {
 
 	@Override
 	public double area() {
-		// TODO Auto-generated method stub
-		return super.area();
+		return super.getAltura() * super.getBase();
+	}
+
+	@Override
+	public double perimetro() {
+		return 2 * super.getAltura() + 2 * super.getBase();
 	}
 	
 	
