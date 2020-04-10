2e9ced0a66b5fc6bb676955ec39fbfcf16d363aa
Jonathan Araujo <57550980+DjowNathan@users.noreply.github.com>
2020-04-10 15:35:16 -0300

    Criação Poligono
    criação de classe

---
+++ src/Poligono.java
@@ -0,0 +1,21 @@
+
+public abstract class Poligono extends Figura {
+	private double base;
+	private double altura;
+	
+	
+	
+	public Poligono(double base, double altura) {
+		this.base = base;
+		this.altura = altura;
+	}
+
+
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
