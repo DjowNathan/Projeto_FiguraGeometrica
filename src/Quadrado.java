85bd468ea2e21205b8b1902286861404a7c27e83
Jonathan Araujo <57550980+DjowNathan@users.noreply.github.com>
2020-04-10 15:43:48 -0300

    Diagonal
    Criação da interface Diagonal e metodo de calcular no quadrado e retangulo

--- src/Quadrado.java
+++ src/Quadrado.java
@@ -1,9 +1,22 @@
 
-public class Quadrado extends Poligono {
+public class Quadrado extends Poligono implements Diagonal {
 
 	public Quadrado(double base, double altura) {
 		super(base, altura);
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public double calcDiagonal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return super.area();
+	}
+	
+	
 }
--- src/Retangulo.java
+++ src/Retangulo.java
@@ -1,9 +1,23 @@
 
-public class Retangulo extends Poligono {
+public class Retangulo extends Poligono implements Diagonal {
 
 	public Retangulo(double base, double altura) {
 		super(base, altura);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public double calcDiagonal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return super.area();
+	}
+	
+	
 
 }
